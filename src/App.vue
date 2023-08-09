bf52610f8498c69fd3f61e8ee2ab482a270cd97b
Oskar Baumann <rakbau@gmail.com>
2023-08-09 15:52:02 -0600

    Remove router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import CodeResults from '@/components/CodeResults.vue'
+import SearchForm from '@/components/SearchForm.vue'
+import Divider from 'primevue/divider'
+
+import SettingsDialog from '@/components/SettingsDialog.vue'
 </script>
 
 <template>
-  <RouterView />
+  <main
+    class="p-component"
+    style="
+      width: 100%;
+      max-width: 800px;
+      margin: 0 auto;
+      display: flex;
+      flex-direction: column;
+      align-items: stretch;
+    "
+  >
+    <SettingsDialog />
+    <SearchForm />
+    <Divider />
+    <CodeResults />
+  </main>
 </template>
-
-<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
-import router from './router'
 import PrimeVue from 'primevue/config'
-
-const app = createApp(App)
-
-app.use(router)
-app.use(PrimeVue)
 
 import 'primevue/resources/themes/saga-blue/theme.css'
 import 'primeicons/primeicons.css'
 
-app.mount('#app')
+createApp(App).use(PrimeVue).mount('#app')
